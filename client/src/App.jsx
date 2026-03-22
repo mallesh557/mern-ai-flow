5666d23c52ae04433d89567ffb3592dc4383320a
malli <malleshpotharaju9@gmail.com>
2026-03-22 19:32:08 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import ReactFlow from "reactflow";
+import "reactflow/dist/style.css";
+import axios from "axios";
+
+export default function App() {
+  const [history, setHistory] = useState([]);
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  
+  // Connect to backend
+  const handleSave = async () => {
+  try {
+    await axios.post("http://localhost:5000/api/save", {
+      prompt: input,
+      response: result,
+    });
+
+    alert("Saved to MongoDB ");
+  } catch (error) {
+    alert("Save failed ");
+  }
+};
+  const handleRun = async () => {
+    try {
+      setLoading(true);
+
+      const res = await axios.post("http://localhost:5000/api/ask-ai", {
+        prompt: input,
+      });
+
+      setResult(res.data.answer);
+    } catch (error) {
+      console.error(error);
+      setResult("Error connecting to backend ");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const nodes = [
+    {
+      id: "1",
+      position: { x: 100, y: 100 },
+      data: {
+        label: (
+          <textarea
+            placeholder="Enter prompt..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            style={{ width: "200px", height: "80px" }}
+          />
+        ),
+      },
+    },
+    {
+      id: "2",
+      position: { x: 400, y: 100 },
+      data: {
+        label: (
+          <div style={{ maxWidth: "200px", minHeight: "80px", border: "1px solid #ccc", padding: "10px" }}>
+            {loading ? "Loading..." : result || "Result will appear here"}
+          </div>
+        ),
+      },
+    },
+  ];
+
+  const edges = [{ id: "e1-2", source: "1", target: "2" }];
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <h2 style={{ textAlign: "center" }}>AI Flow App</h2>
+
+      
+    <button onClick={handleRun}>Run Flow</button>
+<button onClick={handleSave} style={{ marginLeft: "10px" }}>
+  Save
+</button>
+      <ReactFlow nodes={nodes} edges={edges} />
+    </div>
+  );
+}
---
+++ server/server.js
@@ -0,0 +1,103 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch(err => console.log(err));
+
+
+
+  const promptSchema = new mongoose.Schema({
+  prompt: String,
+  response: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Prompt = mongoose.model("Prompt", promptSchema);
+
+app.post("/api/save", async (req, res) => {
+  try {
+    const { prompt, response } = req.body;
+
+    const data = await Prompt.create({
+      prompt,
+      response,
+    });
+
+    res.json({
+      success: true,
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: "Save failed",
+    });
+  }
+});
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+
+
+//  AI Route
+app.post("/api/ask-ai", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+  console.log(prompt)  
+    console.log("Received prompt:", prompt);
+
+  const response = await axios.post(
+  "https://openrouter.ai/api/v1/chat/completions",
+  {
+    model: "openai/gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  },
+  {
+    headers: {
+      Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+      "Content-Type": "application/json",
+      "HTTP-Referer": "http://localhost:5000",
+      "X-Title": "AI Flow App",
+    },
+  }
+);
+    const answer = response.data.choices[0].message.content;
+
+    res.json({
+      success: true,
+      answer: answer,
+    });
+  } catch (error) {
+     console.log("FULL ERROR:", error.response?.data || error.message);
+
+    res.status(500).json({
+      success: false,
+      error: "AI request failed",
+    });
+  }
+});
+
+//  Start Server
+app.listen(5000, () => {
+  console.log("Server running at http://localhost:5000");
+});
